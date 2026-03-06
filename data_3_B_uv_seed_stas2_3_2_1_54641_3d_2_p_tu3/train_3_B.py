93566668fe967e53838cf2628620fb327ee6efec
han-peipei <82298194+han-peipei@users.noreply.github.com>
2026-03-06 23:11:31 +0800

    Update train_3_B.py

--- data_3_B_uv_seed_stas2_3_2_1_54641_3d_2_p_tu3/train_3_B.py
+++ data_3_B_uv_seed_stas2_3_2_1_54641_3d_2_p_tu3/train_3_B.py
@@ -22,7 +22,7 @@ from data_3_B import standardize
 import random
 torch.backends.cuda.matmul.allow_tf32 = False
 torch.backends.cudnn.allow_tf32 = False
-out_dir = "/kaggle/working/2rh/"
+out_dir = "/kaggle/working/uv/"
 os.makedirs(out_dir, exist_ok=True)
 ##########################################################################################
 def stitch_overlapping_forecasts(y_windows):
