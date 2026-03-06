577e9f8163121f93dd12c0b5358feca4479c8198
han-peipei <82298194+han-peipei@users.noreply.github.com>
2026-03-06 18:08:38 +0800

    Update main_3_B.sh

--- data_3_B_uv_seed_stas2_3_2_1_54641_3d_2_p_tu3/main_3_B.sh
+++ data_3_B_uv_seed_stas2_3_2_1_54641_3d_2_p_tu3/main_3_B.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # 可选：线程数（Kaggle 不一定有 64 核）
 # export OMP_NUM_THREADS=8
-path=/kaggle/working/uv
+path=/kaggle/working/2rh
 mkdir -p "$path"
 # 这个脚本所在目录（也就是仓库根目录/或你的代码目录）
 ROOT="$(cd "$(dirname "$0")" && pwd)"
